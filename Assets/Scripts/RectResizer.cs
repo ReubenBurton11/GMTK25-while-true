da23398d2e989f0d1d42067c95d39c5c95bb5a3f
REUBEN BURTON <>
2025-07-31 19:36:54 +0100

    Made draggable resize to text length

--- Assets/Scripts/RectResizer.cs
+++ Assets/Scripts/RectResizer.cs
@@ -1,16 +1,51 @@
 using UnityEngine;
+using TMPro;
 
 public class RectResizer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private TMP_Text text;
+    private RectTransform rect;
+    private RectTransform childRect;
+
+    [SerializeField] private float scale;
+    [SerializeField] private bool textInChild = false;
+
+    private void Update()
     {
-        
+        if (rect != null)
+        {
+            ResizeRect();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        rect = GetComponent<RectTransform>();
+        if (textInChild)
+        {
+            if (text = GetComponentInChildren<TMP_Text>())
+            {
+                childRect = GetComponentInChildren<RectTransform>();
+                ResizeRect();
+            }
+        }
+        else
+        {
+            if (text = GetComponent<TMP_Text>())
+            {
+                ResizeRect();
+            }
+        }
+    }
+
+    void ResizeRect()
+    {
+        int noChars = text.text.Length;
+        float size = noChars * text.fontSize * scale;
+        rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, size);
+        if (textInChild)
+        {
+            childRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, size);
+        }
     }
 }
